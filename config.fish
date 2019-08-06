74e65afab01a8465585532c7a3ce41cc5c589d0c
Phuntsok Drak-pa <phundrak@phundrak.fr>
2019-08-06 13:44:56 +0200

    Added config for Emacs tramp connection in fish

--- config.fish
+++ config.fish
@@ -8,5 +8,14 @@ function fish_title
     true
 end
 
+if test "$TERM" = "dumb"
+    function fish_prompt
+        echo "\$ "
+    end
+    function fish_right_prompt; end
+    function fish_greeting; end
+    function fish_title; end
+end
+
 set -gx PATH $HOME/go/bin $HOME/.cargo/bin $HOME/.local/bin $HOME/.gem/ruby/2.6.0/bin $PATH
 set -gx PKG_CONFIG_PATH /usr/local/lib/pkgconfig/
