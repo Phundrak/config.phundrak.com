75ea8314546b45be08f7340a9ff76c124289f219
Lucien Cartier-Tilet <lucien@phundrak.com>
2020-02-06 23:29:25 +0100

    Added Weather utility

--- .config/fish/functions/we.fish
+++
@@ -1,7 +0,0 @@
-function we -d "Get weather at location"
-    if count $argv > /dev/null
-        curl http://v2.wttr.in/~$argv[1]
-    else
-        curl http://v2.wttr.in/Aubervilliers
-    end
-end
