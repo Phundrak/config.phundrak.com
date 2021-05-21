435adfbb465e22cde46a39d9939145e9dc720fc0
Lucien Cartier-Tilet <lucien@phundrak.com>
2021-05-21 13:59:48 +0200

    Add vanilla Emacs configuration file
    This commit stops tracking my init.el and early-init.el files and
    instead begins tracking my literate org config file for vanilla Emacs.

--- .emacs.vanilla/early-init.el
+++
@@ -1,7 +0,0 @@
-(setq package-enable-at-startup nil)
-(setq inhibit-startup-message t)
-(scroll-bar-mode -1)  ; disable scrollbar
-(tool-bar-mode -1)    ; disable toolbar
-(tooltip-mode -1)     ; disable tooltips
-(set-fringe-mode 10)  ; give some breathing room
-(menu-bar-mode -1)    ; disable menubar
