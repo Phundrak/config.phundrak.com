4c933327cfa29c8b0a45b8f0572097524d873a7d
Lucien Cartier-Tilet <lucien@phundrak.com>
2021-10-14 19:54:02 +0200

    [Misc] Change reference function for potential PDFs

--- org/config/.dir-locals.el
+++ org/config/.dir-locals.el
@@ -1,2 +1,3 @@
-((org-mode . ((org-confirm-babel-evaluate . nil)
-              (eval                       . (writeroom-mode 1)))))
+((org-mode . ((org-confirm-babel-evaluate  . nil)
+              (eval                        . (writeroom-mode 1))
+              (org-latex-reference-command . "\\ref{%s}"))))
