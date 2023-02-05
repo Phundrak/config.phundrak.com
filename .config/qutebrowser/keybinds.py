afc81645ffb69da58752627ff3f31f6952083477
Lucien Cartier-Tilet <lucien@phundrak.com>
2023-02-05 23:07:05 +0100

    [Qutebrowser] Fix spawn in mpv keybind

--- .config/qutebrowser/keybinds.py
+++ .config/qutebrowser/keybinds.py
@@ -1,6 +1,7 @@
 # pylint: disable=C0111
 from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401
 from qutebrowser.config.config import ConfigContainer  # noqa: F401
+
 config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
 c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
 
@@ -198,6 +199,7 @@ config.bind('<Ctrl-F5>', 'reload -f')
 # view
 config.bind(',vh', 'history')
 config.bind(',vs', 'view-source')
+config.bind(",vm", "spawn mpv {url}")
 
 # yank
 config.bind('yd', 'yank domain')
